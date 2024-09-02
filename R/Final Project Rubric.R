d4b3e7729e16088de08f6478be9def3a7415c83f
annatramposch <annatramposch@gmail.com>
2024-09-02 18:20:07 -0400

    re-wrote my figure with ggplot so it was easier to save using the {here} package

--- R/Final Project Rubric.R
+++ R/Final Project Rubric.R
@@ -9,21 +9,21 @@
 
 ## RUBRIC
 
-	# 1 Create a {gtsummary} table of descriptive statistics about your data (1 pt)
-	# 2 Fit a regression and present well-formatted results from the regression (1 pt)
+#	# 1 Create a {gtsummary} table of descriptive statistics about your data (1 pt)
+#	# 2 Fit a regression and present well-formatted results from the regression (1 pt)
 			# The regression doesn’t have to be of any particular scientific interest, and you don’t have to interpret it in any particular way
 			# You may use {broom} or {gtsummary} or both
-	# 3 Create a figure (1 pt)
+#	# 3 Create a figure (1 pt)
 		# It doesn’t need to look pretty; feel free to adapt some of the examples from class, which were as simple as
 			# hist(data$variable) and as complicated as the forest plot in the {broom} section
-	# 4 Write and use a function that does something with the data (1 pt)
+#	# 4 Write and use a function that does something with the data (1 pt)
 		# It could be as simple as, for example, a new function that you write by hand to calculate the standard deviation
-	# Create and render a quarto document that includes at least:
+#	# Create and render a quarto document that includes at least:
 			# 5 The table, regression results, and figure, with appropriate captions (1 pt)
-	# 6 Inline R code in at least 2 places, 1 pulling a statistic from a table (i.e., using gtsummary::inline_text()) and 1 printing something else (like we did with the mean age in the example) (1 pt)
+#	# 6 Inline R code in at least 2 places, 1 pulling a statistic from a table (i.e., using gtsummary::inline_text()) and 1 printing something else (like we did with the mean age in the example) (1 pt)
 	# 7 Cross-references to a table and a figure at least once each (1 pt)
-	# 8 A brief description of the data, including its source (1 pt)
-	# 9 Use the {here} package every time you refer to file paths when reading in data and saving any files (1 pt)
+#	# 8 A brief description of the data, including its source (1 pt)
+#	# 9 Use the {here} package every time you refer to file paths when reading in data and saving any files (1 pt)
 			# You must use it at least twice Feel free to save any files that you create (e.g., your figure) in your code
 	# 10 Use the {renv} package to make sure the teaching team can reproduce your results with the same package versions (1 pt)
 			# You don’t need to include any {renv} code, we can see that you have used it because of the renv.lock file you will include
